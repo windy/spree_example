35d4d430c8c62504a4c129a14d624223abd9db6e
yafeilee <lyfi2003@gmail.com>
2016-05-11 15:01:28 +0800

    deploy file

---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rvm'
+require 'mina/puma'
+
+set :domain, '80percent.io'
+set :deploy_to, '/home/ruby/spree_example'
+set :repository, 'git@github.com:windy/spree_example.git'
+set :branch, 'master'
+
+set :rvm_path, '/usr/local/rvm/scripts/rvm'
+
+set :shared_paths, ['config/database.yml', 'config/application.yml', 'log', 'public/spree']
+
+set :user, 'ruby'
+
+task :environment do
+  invoke :'rvm:use[2.2.3]'
+end
+
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/application.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'application.yml'."]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  to :before_hook do
+    # Put things to run locally before ssh
+  end
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      invoke :'puma:restart'
+    end
+  end
+end
